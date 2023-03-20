3a734feb958bd9d3c66dff309781ff495b10919b
Rishitha005 <rishisamarathunga98@gmail.com>
2023-03-20 21:07:29 +0530

    Path

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="/Portfolio" element={<Home />} />
             <Route path="/projects" element={<ViewAll />} />
           </Route>
         </Routes>
